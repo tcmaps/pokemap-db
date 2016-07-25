ae6b8aae9c42ec69444914684c966f7f05e1982f
TC <tcmaps@gmx.net>
2016-07-26 01:01:23 +0200

    minor text fix

--- db.sql
+++ db.sql
@@ -1,38 +1,45 @@
 -- version info
 CREATE TABLE config
 (
-version varchar(3),
+version varchar(3) DEFAULT '1.0'
 );
 
 -- map cells
 CREATE TABLE cells
 (
-cell_id BIGINT, -- s2sphere format cell id, uint64
-skip BIT(1), -- skip scanning this cell?
-complete_scan BIT(1), -- cell fully traversed each 15min time quadrant?
+cell_id BIGINT UNSIGNED, -- s2sphere format cell id, uint64
+skip BOOLEAN DEFAULT 0, -- skip scanning this cell?
+complete_scan BOOLEAN DEFAULT 0, -- cell fully traversed each 15min time quadrant?
 content BIT(8), -- has (reserved);gyms;pokestops;spawns?
-last_scan TIMESTAMP, -- last scan/update time of this data
+last_scan TIMESTAMP -- last scan/update time of this data
 );
 
 CREATE TABLE forts -- pokestops and gyms
 (
-cell_id BIGINT, -- cell id containing this fort
+cell_id BIGINT UNSIGNED, -- cell id containing this fort
 fort_id VARCHAR(64), -- fort id
-pos_s2 BIGINT, -- s2 level 30 cell id for single value position storage
+pos_s2 BIGINT UNSIGNED, -- s2 level 30 cell id as single value coordinate
 pos_lat DOUBLE, -- wgs84 latitude coordinate stored in iee754 standard
 pos_long DOUBLE, -- wgs84 longtitude coordinate stored in iee754 standard
-fort_type BIT(1), -- gym or pokestop?
-fort_description BLOB, -- (optional) fort description text
+fort_enabled BOOLEAN, -- is fort enabled?
+fort_type TINYINT UNSIGNED, -- gym or pokestop?
+fort_description TEXT, -- (optional) fort description text
 fort_image BLOB, -- (optional) fort image, base64 encoded
-last_scan TIMESTAMP, -- last scan/update time of this data
+fort_sponsor TINYTEXT, -- (optional) fort sponsor? (not used yet)
+fort_last_modified TIMESTAMP, -- (optional) some internal modify time
+last_scan TIMESTAMP -- last scan/update time of this data
 );
 
 CREATE TABLE spawns -- spawn points
 (
-cell_id BIGINT, -- cell id containing this fort
-spawnpoint_id VARCHAR(64), -- spawn point id
-pos_s2 BIGINT, -- level 30 cell id for single value position storage
+cell_id BIGINT UNSIGNED , -- cell id containing this fort
+spawn_id VARCHAR(64) DEFAULT '0', -- spawn point id
+pos_s2 BIGINT UNSIGNED , -- s2 level 30 cell id as single value coordinate
 pos_lat DOUBLE, -- wgs84 latitude coordinate stored in iee754 standard
 pos_long DOUBLE, -- wgs84 longtitude coordinate stored in iee754 standard
-last_scan TIMESTAMP, -- last scan/update time of this data
+static_spawner SMALLINT UNSIGNED , -- is this spawn spawning always the same pokemon? 0 if not
+spawn_time_base TIME DEFAULT 0,
+spawn_time_offset TIME DEFAULT 0,
+spawn_time_dur TIME DEFAULT 0,
+last_scan TIMESTAMP -- last scan/update time of this data
 );
