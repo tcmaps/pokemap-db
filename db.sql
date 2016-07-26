eba882a42fe21d79b45015dee1e26f025154f5fb
TC <tcmaps@gmx.net>
2016-07-26 18:01:30 +0200

    major text fixes

--- db.sql
+++ db.sql
@@ -7,17 +7,18 @@ version varchar(3) DEFAULT '1.0'
 -- map cells
 CREATE TABLE cells
 (
-cell_id BIGINT UNSIGNED, -- s2sphere format cell id, uint64
+cell_id BIGINT UNSIGNED PRIMARY KEY, -- s2sphere format cell id, uint64
 skip BOOLEAN DEFAULT 0, -- skip scanning this cell?
 complete_scan BOOLEAN DEFAULT 0, -- cell fully traversed each 15min time quadrant?
-content BIT(8), -- has (reserved);gyms;pokestops;spawns?
+content BIT(4) DEFAULT b'0000', -- has (reserved);gyms;pokestops;spawns?
 last_scan TIMESTAMP -- last scan/update time of this data
 );
 
 CREATE TABLE forts -- pokestops and gyms
 (
-cell_id BIGINT UNSIGNED, -- cell id containing this fort
+PRIMARY KEY (pos_lat, pos_long),
 fort_id VARCHAR(64), -- fort id
+cell_id BIGINT UNSIGNED, -- s2 level 15 cell id containing this object
 pos_s2 BIGINT UNSIGNED, -- s2 level 30 cell id as single value coordinate
 pos_lat DOUBLE, -- wgs84 latitude coordinate stored in iee754 standard
 pos_long DOUBLE, -- wgs84 longtitude coordinate stored in iee754 standard
@@ -27,19 +28,22 @@ fort_description TEXT, -- (optional) fort description text
 fort_image BLOB, -- (optional) fort image, base64 encoded
 fort_sponsor TINYTEXT, -- (optional) fort sponsor? (not used yet)
 fort_last_modified TIMESTAMP, -- (optional) some internal modify time
-last_scan TIMESTAMP -- last scan/update time of this data
+last_scan TIMESTAMP, -- last scan/update time of this data
+FOREIGN KEY(cell_id) REFERENCES cells(cell_id)
 );
 
 CREATE TABLE spawns -- spawn points
 (
-cell_id BIGINT UNSIGNED , -- cell id containing this fort
+PRIMARY KEY (pos_lat, pos_long),
 spawn_id VARCHAR(64) DEFAULT '0', -- spawn point id
+cell_id BIGINT UNSIGNED, -- s2 level 15 cell id containing this object
 pos_s2 BIGINT UNSIGNED, -- s2 level 30 cell id as single value coordinate
 pos_lat DOUBLE, -- wgs84 latitude coordinate stored in iee754 standard
 pos_long DOUBLE, -- wgs84 longtitude coordinate stored in iee754 standard
 static_spawner SMALLINT UNSIGNED, -- is this spawn spawning always the same pokemon? 0 if not
-spawn_time_base TIME DEFAULT 0, -- the minutes after a full hour the spawn spawns
+spawn_time_base TIME DEFAULT 0, -- the minutes after midnight the spawn spawns
 spawn_time_offset TIME DEFAULT 0, -- minutes until respawn (30,60...180?)
 spawn_time_dur TIME DEFAULT 0, -- spawn duration aka time_till_hidden
-last_scan TIMESTAMP -- last scan/update time of this data
+last_scan TIMESTAMP, -- last scan/update time of this data
+FOREIGN KEY(cell_id) REFERENCES cells(cell_id)
 );
